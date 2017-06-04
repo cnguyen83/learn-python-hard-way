151407b4e36fe64a1a36ea6f588e874868ab38aa
Christina Nguyen <cq_nguyen_vn@yahoo.com>
2017-06-04 00:36:51 -0600

    Add ex7.py

---
+++ ex7.py
@@ -0,0 +1,30 @@
+# Print Mary had a little lamb lyrics
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+
+# A string can be multiplied,
+# which simply repeats the string.
+print "." * 10
+
+# Variables that break up 'Cheese Burger'
+# into individual characters
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Strings can be concatenated together.
+# When using 'print,' the comma at the end
+# indicates that the next 'print' statement
+# should be printed on the same line.
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
