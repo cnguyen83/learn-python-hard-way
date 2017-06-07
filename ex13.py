9babfe878cf6b9086efb54d9a562dd9688fee23d
Christina Nguyen <cq_nguyen_vn@yahoo.com>
2017-06-06 23:18:45 -0600

    Update ex13.py

--- ex13.py
+++ ex13.py
@@ -1,5 +1,6 @@
 # This imports the sys module
-# so we can take in args at the initialization.
+# so we can take in args at initialization
+# from the command line.
 from sys import argv
 
 # Unpacks argv args into variables
@@ -11,7 +12,7 @@ print "Your first variable is:", first
 print "Your second variable is:", second
 print "Your third variable is:", third
 
-# Now take in user input from command line
+# Now take in user input from prompt
 # as raw_input()
 fourth = raw_input("Do you have a fourth variable? ")
 
